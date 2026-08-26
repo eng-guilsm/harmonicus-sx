97e722af7bbdd12ee389c155a2e2a5acfdfb8737
Guilherme Santos <g.s.macedo7@gmail.com>
2026-08-26 13:13:50 +0000

    Auto-Update (1m): 2026-08-26 13:13:39

--- data/planos_data.js
+++ data/planos_data.js
@@ -1,6 +1,6 @@
-// Gerado automaticamente pelo Harmonicus SX Engine (2026-08-26 10:12:28)
+// Gerado automaticamente pelo Harmonicus SX Engine (2026-08-26 10:13:42)
 window.PORTFOLIO_STATE = {
-  "timestamp_str": "2026-08-26 10:12:28",
+  "timestamp_str": "2026-08-26 10:13:42",
   "total_brl": 1562.57,
   "aportado_brl": 1712.91,
   "lucro_liquido_real_brl": -150.34,
