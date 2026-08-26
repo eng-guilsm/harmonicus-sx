b97e07767525e1b4cf19d31c4734d9fb2b818291
Guilherme Santos <g.s.macedo7@gmail.com>
2026-08-26 08:59:12 +0000

    Auto-Update (1m): 2026-08-26 08:59:01

--- data/charts_data.js
+++ data/charts_data.js
@@ -1,4 +1,4 @@
-// Gerado automaticamente pelo MoneyLab Multi-Timeframe Kinetics Engine (2026-08-26 08:57:56)
+// Gerado automaticamente pelo MoneyLab Multi-Timeframe Kinetics Engine (2026-08-26 08:59:11)
 window.ASSETS_KINETICS_DATA = {
   "BTCBRL": {
     "symbol": "BTCBRL",
