c88b4a75b4b6d82932236767b21265ce836a8777
Guilherme Santos <g.s.macedo7@gmail.com>
2026-08-26 03:59:12 +0000

    Auto-Update (1m): 2026-08-26 03:59:00

--- data/charts_data.js
+++ data/charts_data.js
@@ -1,4 +1,4 @@
-// Gerado automaticamente pelo MoneyLab Multi-Timeframe Kinetics Engine (2026-08-26 03:57:57)
+// Gerado automaticamente pelo MoneyLab Multi-Timeframe Kinetics Engine (2026-08-26 03:59:11)
 window.ASSETS_KINETICS_DATA = {
   "BTCBRL": {
     "symbol": "BTCBRL",
--- data/planos_data.js
+++ data/planos_data.js
@@ -1,6 +1,6 @@
-// Gerado automaticamente pelo Harmonicus SX Engine (2026-08-26 00:57:50)
+// Gerado automaticamente pelo Harmonicus SX Engine (2026-08-26 00:59:04)
 window.PORTFOLIO_STATE = {
-  "timestamp_str": "2026-08-26 00:57:50",
+  "timestamp_str": "2026-08-26 00:59:04",
   "total_brl": 1562.67,
   "aportado_brl": 1712.91,
   "lucro_liquido_real_brl": -150.24,
